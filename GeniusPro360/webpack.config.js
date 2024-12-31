1d013c5ce5248aee48be4656b2a14d9fa5b299fa
abondiaye <ndiayeaboulaye657@gmail.com>
2024-12-31 11:41:26 +0100

    add: interface

--- GeniusPro360/webpack.config.js
+++ GeniusPro360/webpack.config.js
@@ -3,30 +3,29 @@ const Encore = require('@symfony/webpack-encore');
 Encore
     // Répertoire où les fichiers compilés seront stockés
     .setOutputPath('public/build/')
+    
     // Chemin public utilisé par le serveur pour accéder aux fichiers
     .setPublicPath('/build')
-    
-    // Entrée pour le JavaScript principal
-    .addEntry('app', './src/app.js')
-    
-    // Entrée pour les styles CSS/SCSS
-    .addStyleEntry('appStyles', './assets/styles/app.scss')
-    
+
+    // Entrée principale pour les fichiers JS
+    .addEntry('app', './assets/app.js')
+
+    // Entrée principale pour les fichiers CSS/SCSS
+    .addStyleEntry('styles', './assets/styles/styles.scss')
+
     // Activer le chunk d'exécution unique
     .enableSingleRuntimeChunk()
-    
-    // Activer le support pour Sass
+
+    // Activer le support pour Sass/SCSS
     .enableSassLoader()
-    
+
     // Nettoie le répertoire de sortie avant chaque build
     .cleanupOutputBeforeBuild()
-    
+
     // Versionning des fichiers pour le cache-busting en production
     .enableVersioning(Encore.isProduction())
 
     // Notifications de build
     .enableBuildNotifications();
-
-    
 
 module.exports = Encore.getWebpackConfig();
